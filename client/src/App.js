95154fd621debabb0962131a6cef9aa5c272d4ac
Noel <noelsobryant@gmail.com>
2022-12-22 05:09:51 +0300

    added routes and created components and pages folders

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,25 @@
-import logo from "./logo.svg";
-import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Article from "./pages/Article";
+import ArticleList from "./pages/ArticleList";
+import Navbar from "./components/Navbar";
 
 function App() {
-  return <div className="bg-red-500 text-center">My Blog</div>;
+  return (
+    <Router>
+      <Navbar />
+      <div className="max-w-screen-md mx-auto pt-20">
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/about" element={<About />}></Route>
+          <Route path="/article" element={<Article />}></Route>
+          <Route path="/article-list" element={<ArticleList />}></Route>
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
---
+++ client/src/pages/Home.js
@@ -0,0 +1,42 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="mb-20">
+      <h1 className="sm:text-4xl text-2xl font-bold  my-6 text-gray-900">
+        Hello, Welcome to my Blog
+      </h1>
+      <p className="mx-auto leading-relaxed text-base  mb-4">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum quod
+        aliquid a omnis minima. Optio nihil, similique quisquam quidem velit
+        dicta expedita architecto iusto fuga neque in sed error voluptates
+        placeat tempore atque minima? Quo dolores beatae praesentium laborum
+        sequi cumque quam, inventore suscipit, quaerat quidem molestiae
+        reprehenderit autem quisquam!
+      </p>
+      <p className="mx-auto leading-relaxed text-base  mb-4">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum quod
+        aliquid a omnis minima. Optio nihil, similique quisquam quidem velit
+        dicta expedita architecto iusto fuga neque in sed error voluptates
+        placeat tempore atque minima? Quo dolores beatae praesentium laborum
+        sequi cumque quam, inventore suscipit, quaerat quidem molestiae
+        reprehenderit autem quisquam!
+      </p>
+      <p className="mx-auto leading-relaxed text-base  mb-4">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam sapiente
+        voluptatum blanditiis esse necessitatibus dignissimos, deserunt culpa
+        odit nemo, incidunt cupiditate possimus a sed suscipit! Nulla, enim cum
+        quis in veritatis animi non eveniet, ut minima tenetur, dolorem
+        perferendis deserunt ipsum quod. Ducimus, reiciendis! Libero culpa
+        impedit quisquam veniam tempora iste, voluptas quia reprehenderit labore
+        vitae! Eius obcaecati, error provident pariatur, fugiat sit quibusdam
+        blanditiis odio dignissimos corporis ex exercitationem nam, quidem a?
+        Velit qui eaque, ad dolorem molestias minima consequuntur et vel earum
+        cupiditate recusandae, deserunt sit est quisquam corrupti dolores
+        tempora. Obcaecati, quis. Dolorem sunt asperiores odio tempore?
+      </p>
+    </div>
+  );
+};
+
+export default Home;
